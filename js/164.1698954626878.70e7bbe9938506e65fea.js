6c36e01e050121b2eb3627faf3b8cb09b46050a7
Travis CI User <travis@example.org>
2023-11-02 19:53:03 +0000

    Merge pull request #366 from RedHatInsights/dependabot/npm_and_yarn/postcss-8.4.31
    Bump postcss from 8.3.6 to 8.4.31

---
+++ js/164.1698954626878.70e7bbe9938506e65fea.js
@@ -0,0 +1,1 @@
+(self.webpackChunkpolicies_ui_frontend=self.webpackChunkpolicies_ui_frontend||[]).push([[164,730],{22868:()=>{},14777:()=>{},99830:()=>{},70209:()=>{},87414:()=>{}}]);
---
+++ js/730.1698954626878.70e7bbe9938506e65fea.js
@@ -0,0 +1,1 @@
+(self.webpackChunkpolicies_ui_frontend=self.webpackChunkpolicies_ui_frontend||[]).push([[730,164],{22868:()=>{},14777:()=>{},99830:()=>{},70209:()=>{},87414:()=>{}}]);
